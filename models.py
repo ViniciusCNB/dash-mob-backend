e245afe5dd164a56a12912215b9693e2f0298ee3
Vinícius Borges <viniborg25@gmail.com>
2025-06-24 18:04:26 -0300

    change to FastAPI framework

--- models.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-
-
-class ViagensPorLinha(db.Model):
-    __tablename__ = "viagens_por_linha"
-    id = db.Column(db.Integer, primary_key=True)
-    linha = db.Column(db.String())
-    quantidade_viagens = db.Column(db.BigInteger())
